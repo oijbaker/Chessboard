85113ebbc3e2f4f1460667ea0d27c5a541129e00
Oliver Baker <oijbaker@outlook.com>
2021-01-27 14:03:01 +0000

    Update frontend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Chess Goat</h1>
+    <Chessboard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Chessboard from './templates/chessboard.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Chessboard
   }
 }
 </script>
@@ -24,5 +25,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  color:"Red";
 }
 </style>
---
+++ frontend/src/templates/chessboard.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img style="position: relative" src="../assets/chessboard.png">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Chessboard",
+    props: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
